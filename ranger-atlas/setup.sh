a41ebcd45d3a68c5ef70f4cf486f1a6c28a88f71
Ali Bajwa <abajwa@hortonworks.com>
2017-06-27 19:02:46 -0700

    added sleep after atlas comes up

--- ranger-atlas/setup.sh
+++ ranger-atlas/setup.sh
@@ -335,6 +335,7 @@ EOF
 
     sleep 20
     while ! echo exit | nc localhost 21000; do echo "waiting for atlas to come up..."; sleep 10; done
+    sleep 30
 
     # curl -u admin:${ambari_pass} -i -H 'X-Requested-By: blah' -X POST -d '{"RequestInfo": {"context" :"ATLAS Service Check","command":"ATLAS_SERVICE_CHECK"},"Requests/resource_filters":[{"service_name":"ATLAS"}]}' http://localhost:8080/api/v1/clusters/${cluster_name}/requests
     
