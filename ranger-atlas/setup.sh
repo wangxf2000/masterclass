b2baf53beb17c6cd8813320bd9e8db82505c8116
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2017-09-13 16:48:10 -0700

    Update setup.sh

--- ranger-atlas/setup.sh
+++ ranger-atlas/setup.sh
@@ -213,6 +213,8 @@ cat << EOF > configuration-custom.json
 EOF
 
     sleep 40
+    service ambari-server status
+    curl -u admin:${ambari_pass} -i -H "X-Requested-By: blah" -X GET ${ambari_url}/hosts
     ./deploy-recommended-cluster.bash
 
     if [ "${deploy}" = "true" ]; then
