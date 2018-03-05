4cc51eb52b14146fb46b2131c0df213034ff6114
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2018-03-05 14:52:09 -0800

    added logging

--- ranger-atlas/setup_sandbox.sh
+++ ranger-atlas/setup_sandbox.sh
@@ -23,9 +23,9 @@ output=`curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari'  http://${ho
 cluster_name=`echo $output | sed -n 's/.*"cluster_name" : "\([^\"]*\)".*/\1/p'`
 
 
-####### Configure cluster for demo
+echo "####### Configure cluster for demo..."
 
-#stop Flume, oozie, spark2 and put in maintenance mode
+echo stop Flume, oozie, spark2 and put in maintenance mode
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Stop FALCON via REST"}, "Body": {"ServiceInfo": {"state": "INSTALLED"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/FLUME
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Stop OOZIE via REST"}, "Body": {"ServiceInfo": {"state": "INSTALLED"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/OOZIE
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Stop SPARK2 via REST"}, "Body": {"ServiceInfo": {"state": "INSTALLED"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/SPARK2
@@ -37,29 +37,29 @@ while echo exit | nc ${host} 18081; do echo "waiting for Spark2 to go down..."; 
 
 
 
-#make sure kafka, hbase, ambari infra, atlas, HDFS are out of maintenance mode
+echo make sure kafka, hbase, ambari infra, atlas, HDFS are out of maintenance mode
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Remove Kafka from maintenance mode"}, "Body": {"ServiceInfo": {"maintenance_state": "OFF"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/KAFKA
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Remove Hbase from maintenance mode"}, "Body": {"ServiceInfo": {"maintenance_state": "OFF"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/HBASE
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Remove Ambari Infra from maintenance mode"}, "Body": {"ServiceInfo": {"maintenance_state": "OFF"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/AMBARI_INFRA
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Remove Atlas from maintenance mode"}, "Body": {"ServiceInfo": {"maintenance_state": "OFF"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/ATLAS
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Remove HDFS from maintenance mode"}, "Body": {"ServiceInfo": {"maintenance_state": "OFF"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/HDFS
 
-## Ranger config changes
+echo # Ranger config changes
 
-#Enable kafka plugin for Ranger 
+echo Enable kafka plugin for Ranger 
 /var/lib/ambari-server/resources/scripts/configs.py -u admin -p ${ambari_pass} --host ${host} --port 8080 --cluster ${cluster_name} -a set -c ranger-env -k ranger-kafka-plugin-enabled -v Yes
 /var/lib/ambari-server/resources/scripts/configs.py -u admin -p ${ambari_pass} --host ${host} --port 8080 --cluster ${cluster_name} -a set -c ranger-kafka-plugin-properties -k ranger-kafka-plugin-enabled -v Yes
 
-#enable Audits for Ranger
+echo enable Audits for Ranger
 /var/lib/ambari-server/resources/scripts/configs.py -u admin -p ${ambari_pass} --host ${host} --port 8080 --cluster ${cluster_name} -a set -c ranger-env -k xasecure.audit.destination.solr -v true
 
-#enable other plugin audits
+echo enable other plugin audits
 /var/lib/ambari-server/resources/scripts/configs.py -u admin -p ${ambari_pass} --host ${host} --port 8080 --cluster ${cluster_name} -a set -c ranger-hdfs-audit -k xasecure.audit.destination.solr -v true
 /var/lib/ambari-server/resources/scripts/configs.py -u admin -p ${ambari_pass} --host ${host} --port 8080 --cluster ${cluster_name} -a set -c ranger-atlas-audit -k xasecure.audit.destination.solr -v true
 /var/lib/ambari-server/resources/scripts/configs.py -u admin -p ${ambari_pass} --host ${host} --port 8080 --cluster ${cluster_name} -a set -c ranger-kafka-audit -k xasecure.audit.destination.solr -v true
 /var/lib/ambari-server/resources/scripts/configs.py -u admin -p ${ambari_pass} --host ${host} --port 8080 --cluster ${cluster_name} -a set -c ranger-hbase-audit -k xasecure.audit.destination.solr -v true
 
-#Ranger tagsync mappings
+echo Ranger tagsync mappings
 /var/lib/ambari-server/resources/scripts/configs.py -u admin -p ${ambari_pass} --host ${host} --port 8080 --cluster ${cluster_name} -a set -c ranger-tagsync-site -k ranger.tagsync.atlas.hdfs.instance.hdp.ranger.service -v ${cluster_name}_hadoop
 /var/lib/ambari-server/resources/scripts/configs.py -u admin -p ${ambari_pass} --host ${host} --port 8080 --cluster ${cluster_name} -a set -c ranger-tagsync-site -k ranger.tagsync.atlas.hive.instance.hdp.ranger.service -v ${cluster_name}_hive
 /var/lib/ambari-server/resources/scripts/configs.py -u admin -p ${ambari_pass} --host ${host} --port 8080 --cluster ${cluster_name} -a set -c ranger-tagsync-site -k ranger.tagsync.atlas.hbase.instance.hdp.ranger.service -v ${cluster_name}_hbase
@@ -70,27 +70,27 @@ curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"Request
 /var/lib/ambari-server/resources/scripts/configs.py -u admin -p ${ambari_pass} --host ${host} --port 8080 --cluster ${cluster_name} -a set -c ranger-tagsync-site -k ranger.tagsync.atlas.tag.instance.hdp.ranger.service -v tags
 
 
-#stop Ranger
+echo stop Ranger
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Stop RANGER via REST"}, "Body": {"ServiceInfo": {"state": "INSTALLED"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/RANGER
 while echo exit | nc ${host} 6080; do echo "waiting for Ranger to go down..."; sleep 10; done
 sleep 10
 
-#start Ranger
+echo start Ranger
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start RANGER via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/RANGER
 
 #curl  -u admin:${ambari_pass} -H "X-Requested-By: ambari" -X POST  -d '{"RequestInfo":{"command":"RESTART","context":"Restart all required services","operation_level":"host_component"},"Requests/resource_filters":[{"hosts_predicate":"HostRoles/stale_configs=true"}]}' http://${host}:8080/api/v1/clusters/${cluster_name}/requests
 #sleep 20
 
 
-#wait until ranger comes up
+echo wait until ranger comes up
 while ! echo exit | nc ${host} 6080; do echo "waiting for Ranger to come up..."; sleep 10; done
 
 
-#Change Hive doAs setting and enable audits
+echo Change Hive doAs setting and enable audits
 /var/lib/ambari-server/resources/scripts/configs.py -u admin -p ${ambari_pass} --host ${host} --port 8080 --cluster ${cluster_name} -a set -c hive-site -k hive.server2.enable.doAs  -v true
 /var/lib/ambari-server/resources/scripts/configs.py -u admin -p ${ambari_pass} --host ${host} --port 8080 --cluster ${cluster_name} -a set -c ranger-hive-audit -k xasecure.audit.destination.solr -v true
 
-#restart Hive
+echo restart Hive
 
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Stop HIVE via REST"}, "Body": {"ServiceInfo": {"state": "INSTALLED"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/HIVE
 while echo exit | nc ${host} 10000; do echo "waiting for Hive to go down..."; sleep 10; done
@@ -99,13 +99,13 @@ sleep 15
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start HIVE via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/HIVE
 
 
-#Start Kafka, HBase, Ambari infra, Atlas
+echo Start Kafka, HBase, Ambari infra, Atlas
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start KAFKA via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/KAFKA
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start HBASE via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/HBASE
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start Ambari infra via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/AMBARI_INFRA
 curl -u admin:${ambari_pass} -i -H 'X-Requested-By: ambari' -X PUT -d '{"RequestInfo": {"context" :"Start Atlas via REST"}, "Body": {"ServiceInfo": {"state": "STARTED"}}}' http://${host}:8080/api/v1/clusters/${cluster_name}/services/ATLAS
 
-#wait until atlas come up
+echo wait until atlas come up
 while ! echo exit | nc ${host} 21000; do echo "waiting for atlas to come up..."; sleep 10; done
 
 
@@ -115,17 +115,17 @@ while ! echo exit | nc ${host} 21000; do echo "waiting for atlas to come up...";
 #/usr/lib/ambari-infra-solr-client/solrCloudCli.sh --zookeeper-connect-string ${host}:2181/infra-solr --create-collection --collection ranger_audits --config-set ranger_audits --shards 1 --replication 1 --max-shards 1 --retry 5 --interval 10
 
 
-####### Start HortoniaBank demo setup
+echo ###### Start HortoniaBank demo setup
 
 
-#download hortonia scripts
+echo download hortonia scripts
 cd /tmp
 git clone https://github.com/abajwa-hw/masterclass  
 
 cd /tmp/masterclass/ranger-atlas/HortoniaMunichSetup
 ./04-create-os-users.sh    
 
-#also need anonymous user for kafka Ranger policy
+echo also need anonymous user for kafka Ranger policy
 useradd ANONYMOUS    
 
 users="kate_hr ivanna_eu_hr joe_analyst sasha_eu_hr john_finance mark_bizdev jermy_contractor diane_csr log_monitor"
@@ -137,46 +137,46 @@ for user in ${users}; do
   curl -u admin:${ambari_pass} -H "X-Requested-By: blah" -X POST -d "{\"Users/user_name\":\"${user}\",\"Users/password\":\"${ambari_pass}\",\"Users/active\":\"true\",\"Users/admin\":\"false\"}" ${ambari_url}/users 
 done 
 
-#create groups in Ambari
+echo create groups in Ambari
 for group in ${groups}; do
   curl -u admin:${ambari_pass} -H "X-Requested-By: blah" -X POST -d "{\"Groups/group_name\":\"${group}\"}" ${ambari_url}/groups
 done
 
-#HR group membership
+echo HR group membership
 curl -u admin:${ambari_pass} -H "X-Requested-By: blah" -X POST -d '{"MemberInfo/user_name":"kate_hr", "MemberInfo/group_name":"hr"}' ${ambari_url}/groups/hr/members
 curl -u admin:${ambari_pass} -H "X-Requested-By: blah" -X POST -d '{"MemberInfo/user_name":"ivanna_eu_hr", "MemberInfo/group_name":"hr"}' ${ambari_url}/groups/hr/members
 curl -u admin:${ambari_pass} -H "X-Requested-By: blah" -X POST -d '{"MemberInfo/user_name":"sasha_eu_hr", "MemberInfo/group_name":"hr"}' ${ambari_url}/groups/hr/members
 
 
-#analyst group membership
+echo analyst group membership
 curl -u admin:${ambari_pass} -H "X-Requested-By: blah" -X POST -d '{"MemberInfo/user_name":"joe_analyst", "MemberInfo/group_name":"analyst"}' ${ambari_url}/groups/analyst/members
 
-#us_employee group membership
+echo us_employee group membership
 curl -u admin:${ambari_pass} -H "X-Requested-By: blah" -X POST -d '{"MemberInfo/user_name":"kate_hr", "MemberInfo/group_name":"us_employee"}' ${ambari_url}/groups/us_employee/members
 curl -u admin:${ambari_pass} -H "X-Requested-By: blah" -X POST -d '{"MemberInfo/user_name":"joe_analyst", "MemberInfo/group_name":"us_employee"}' ${ambari_url}/groups/us_employee/members
 
-#eu_employee group membership
+echo eu_employee group membership
 curl -u admin:${ambari_pass} -H "X-Requested-By: blah" -X POST -d '{"MemberInfo/user_name":"ivanna_eu_hr", "MemberInfo/group_name":"eu_employee"}' ${ambari_url}/groups/eu_employee/members
 curl -u admin:${ambari_pass} -H "X-Requested-By: blah" -X POST -d '{"MemberInfo/user_name":"sasha_eu_hr", "MemberInfo/group_name":"eu_employee"}' ${ambari_url}/groups/eu_employee/members
 
-#finance group membership
+echo finance group membership
 curl -u admin:${ambari_pass} -H "X-Requested-By: blah" -X POST -d '{"MemberInfo/user_name":"john_finance", "MemberInfo/group_name":"finance"}' ${ambari_url}/groups/finance/members
 
-#bizdev group membership
+echo bizdev group membership
 curl -u admin:${ambari_pass} -H "X-Requested-By: blah" -X POST -d '{"MemberInfo/user_name":"mark_bizdev", "MemberInfo/group_name":"business_dev"}' ${ambari_url}/groups/business_dev/members
 
-#contractor group membership
+echo contractor group membership
 curl -u admin:${ambari_pass} -H "X-Requested-By: blah" -X POST -d '{"MemberInfo/user_name":"jermy_contractor", "MemberInfo/group_name":"contractor"}' ${ambari_url}/groups/contractor/members
 
-#csr group membership
+echo csr group membership
 curl -u admin:${ambari_pass} -H "X-Requested-By: blah" -X POST -d '{"MemberInfo/user_name":"diane_csr", "MemberInfo/group_name":"csr"}' ${ambari_url}/groups/csr/members
 
-#csr group membership
+echo csr group membership
 curl -u admin:${ambari_pass} -H "X-Requested-By: blah" -X POST -d '{"MemberInfo/user_name":"log_monitor", "MemberInfo/group_name":"etluser"}' ${ambari_url}/groups/etluser/members
 	
 
 
-#add groups to Hive views
+echo add groups to Hive views
 curl -u admin:${ambari_pass} -i -H "X-Requested-By: blah" -X PUT ${ambari_url}/views/HIVE/versions/1.5.0/instances/AUTO_HIVE_INSTANCE/privileges \
    --data '[{"PrivilegeInfo":{"permission_name":"VIEW.USER","principal_name":"us_employee","principal_type":"GROUP"}},{"PrivilegeInfo":{"permission_name":"VIEW.USER","principal_name":"business_dev","principal_type":"GROUP"}},{"PrivilegeInfo":{"permission_name":"VIEW.USER","principal_name":"eu_employee","principal_type":"GROUP"}},{"PrivilegeInfo":{"permission_name":"VIEW.USER","principal_name":"CLUSTER.ADMINISTRATOR","principal_type":"ROLE"}},{"PrivilegeInfo":{"permission_name":"VIEW.USER","principal_name":"CLUSTER.OPERATOR","principal_type":"ROLE"}},{"PrivilegeInfo":{"permission_name":"VIEW.USER","principal_name":"SERVICE.OPERATOR","principal_type":"ROLE"}},{"PrivilegeInfo":{"permission_name":"VIEW.USER","principal_name":"SERVICE.ADMINISTRATOR","principal_type":"ROLE"}},{"PrivilegeInfo":{"permission_name":"VIEW.USER","principal_name":"CLUSTER.USER","principal_type":"ROLE"}}]'
 
@@ -186,16 +186,16 @@ curl -u admin:${ambari_pass} -i -H 'X-Requested-By: blah' -X PUT ${ambari_url}/v
 
 
 
-#pull latest notebooks
+echo pull latest notebooks
 curl -sSL https://raw.githubusercontent.com/hortonworks-gallery/zeppelin-notebooks/master/update_all_notebooks.sh | sudo -E sh 
 
-#make sure HDFS is up
+echo make sure HDFS is up
 while ! echo exit | nc ${host} 50070; do echo "waiting for HDFS to come up..."; sleep 10; done
 
 sudo -u zeppelin hdfs dfs -rmr /user/zeppelin/notebook/*
 sudo -u zeppelin hdfs dfs -put /usr/hdp/current/zeppelin-server/notebook/* /user/zeppelin/notebook/
 
-#disable anonymous login and create Hortonia users
+echo disable anonymous login and create Hortonia users
 cat << EOF > /tmp/zeppelin-env.json
 {
   "properties": {
@@ -208,20 +208,20 @@ EOF
 sleep 5
 
 
-#make sure YARN up
+echo make sure YARN up
 while ! echo exit | nc ${host} 8088; do echo "waiting for YARN to come up..."; sleep 10; done
 
-#kill any previous Hive/tez apps to clear queue
+echo kill any previous Hive/tez apps to clear queue
 for app in $(yarn application -list | awk '$2==hive && $3==TEZ && $6 == "ACCEPTED" || $6 == "RUNNING" { print $1 }')
 do 
 	yarn application -kill  "$app"
 done
 
 
-#make sure Ranger up
+echo make sure Ranger up
 while ! echo exit | nc ${host} 6080; do echo "waiting for Ranger to come up..."; sleep 10; done
 
-## update ranger to support deny policies
+echo update ranger to support deny policies
 ranger_curl="curl -u admin:admin"
 ranger_url="http://${host}:6080/service"
 
@@ -294,13 +294,13 @@ cd /tmp/masterclass/ranger-atlas/HortoniaMunichSetup
 su hdfs -c ./05-create-hdfs-user-folders.sh
 su hdfs -c ./06-copy-data-to-hdfs.sh
 
-#make sure hive is up
+echo make sure hive is up
 while ! echo exit | nc ${host} 10000; do echo "waiting for hive to come up..."; sleep 10; done
 
 ./07-create-hive-schema.sh
  		  
 
-#enable kerberos
+echo enable kerberos
 
 cd /tmp
 git clone https://github.com/crazyadmins/useful-scripts.git
@@ -320,12 +320,12 @@ chmod +x setup_kerberos.sh
 ./setup_kerberos.sh 
 
 
-#make sure Atlas/Hive are up
+echo make sure Atlas/Hive are up
 while ! echo exit | nc ${host} 21000; do echo "waiting for atlas to come up..."; sleep 10; done
 while ! echo exit | nc ${host} 10000; do echo "waiting for hive to come up..."; sleep 10; done
 
 
-#Now that we enabled Kafka Ranger plugin (by enabling kerberos), import Atlas entities
+echo Now that we enabled Kafka Ranger plugin (by enabling kerberos), import Atlas entities
 cd /tmp/masterclass/ranger-atlas/HortoniaMunichSetup
 ./02-atlas-import-entities.sh
 # Need to do this twice due to bug: RANGER-1897 
@@ -336,4 +336,3 @@ cd /tmp/masterclass/ranger-atlas/HortoniaMunichSetup
 echo "Automated portion of setup is complete, next please create the tag repo in Ranger, associate with Hive and import tag policies"
 echo "See https://github.com/abajwa-hw/masterclass/blob/master/ranger-atlas/README.md for more details"
 echo "Once complete, see here for walk through of demo: https://community.hortonworks.com/articles/151939/hdp-securitygovernance-demo-kit.html"
-
