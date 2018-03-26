9d7153580affa41c184a4c2dd3834c5e712d777f
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2018-03-26 00:57:23 -0700

    create users/keytabs

--- ranger-atlas/setup.sh
+++ ranger-atlas/setup.sh
@@ -478,7 +478,31 @@ EOF
     # second time, the notification is of type ENTITY_UPDATE which gets processed correctly
     ./02-atlas-import-entities.sh
     
-                
+    echo "Creating users in KDC..."
+    kadmin.local -q "addprinc -randkey joe_analyst/$(hostname -f)@${kdc_realm}"
+    kadmin.local -q "addprinc -randkey kate_hr/$(hostname -f)@${kdc_realm}"
+    kadmin.local -q "addprinc -randkey log_monitor/$(hostname -f)@${kdc_realm}"
+    kadmin.local -q "addprinc -randkey diane_csr/$(hostname -f)@${kdc_realm}"
+    kadmin.local -q "addprinc -randkey jermy_contractor/$(hostname -f)@${kdc_realm}"
+    kadmin.local -q "addprinc -randkey mark_bizdev/$(hostname -f)@${kdc_realm}"
+    kadmin.local -q "addprinc -randkey john_finance/$(hostname -f)@${kdc_realm}"
+    kadmin.local -q "addprinc -randkey ivanna_eu_hr/$(hostname -f)@${kdc_realm}"
+
+
+    echo "Creating user keytabs..."
+    kadmin.local -q "xst -k joe_analyst.keytab joe_analyst/$(hostname -f)@${kdc_realm}"    
+    kadmin.local -q "xst -k log_monitor.keytab log_monitor/$(hostname -f)@${kdc_realm}"
+    kadmin.local -q "xst -k diane_csr.keytab diane_csr/$(hostname -f)@${kdc_realm}"
+    kadmin.local -q "xst -k jermy_contractor.keytab jermy_contractor/$(hostname -f)@${kdc_realm}"
+    kadmin.local -q "xst -k mark_bizdev.keytab mark_bizdev/$(hostname -f)@${kdc_realm}"
+    kadmin.local -q "xst -k john_finance.keytab john_finance/$(hostname -f)@${kdc_realm}"
+    kadmin.local -q "xst -k ivanna_eu_hr.keytab ivanna_eu_hr/$(hostname -f)@${kdc_realm}"
+    kadmin.local -q "xst -k kate_hr.keytab kate_hr/$(hostname -f)@${kdc_realm}"
+
+    mv *.keytab /etc/security/keytabs
+
+
+
     fi
 fi
 
