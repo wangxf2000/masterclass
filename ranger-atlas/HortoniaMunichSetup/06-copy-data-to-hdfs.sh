15181b7b4e5898b9064ae317eb4ef4b339455457
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2018-03-26 00:38:43 -0700

    added commands to create hbase tables/kafka topics

--- ranger-atlas/HortoniaMunichSetup/06-copy-data-to-hdfs.sh
+++ ranger-atlas/HortoniaMunichSetup/06-copy-data-to-hdfs.sh
@@ -1,3 +1,4 @@
+echo "Copying data to /hive_data dir in HDFS..."
 hdfs dfs -mkdir -p /hive_data/claim/
 hdfs dfs -mkdir -p /hive_data/cost_savings/
 hdfs dfs -mkdir -p /hive_data/finance/tax_2009/
@@ -17,3 +18,15 @@ hdfs dfs -put data/us_customers_data.csv            /hive_data/hortoniabank/us_c
 hdfs dfs -put data/ww_customers_data.csv            /hive_data/hortoniabank/ww_customers/
 
 hdfs dfs -chown -R hive:hive /hive_data/
+
+
+echo "Creating Hbase tables..."
+echo "create 'T_PRIVATE','cf1','cf2'" | hbase shell
+echo "create 'T_FOREX','cf1','cf2'" | hbase shell
+echo "list" | hbase shell
+
+echo "Creating Kafka topics..."
+/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --zookeeper $(hostname -f):2181 --replication-factor 1 --partition 1 --topic FOREX
+/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --zookeeper $(hostname -f):2181 --replication-factor 1 --partition 1 --topic PRIVATE
+/usr/hdp/current/kafka-broker/bin/kafka-topics.sh --zookeeper $(hostname -f):2181 --list
+
