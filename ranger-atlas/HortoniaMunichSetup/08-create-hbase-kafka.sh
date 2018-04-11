bb9bc6bc2d51896a0d47bc7bcde6fb0cf1e22aa9
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2018-04-11 16:39:39 -0700

    updated to support security

--- ranger-atlas/HortoniaMunichSetup/08-create-hbase-kafka.sh
+++ ranger-atlas/HortoniaMunichSetup/08-create-hbase-kafka.sh
@@ -1,5 +1,16 @@
+#if kafka broker var is set, use it. Else use current host
+kafka_broker=${kafka_broker:-$(hostname -f)}
+
+echo "kafka_broker detected as ${kafka_broker}"
+echo "enable_kerberos detected as ${enable_kerberos}"
+echo "kdc_realm detected as ${kdc_realm}"
+
+if [ "${enable_kerberos}" = true  ]; then   
+   kinit -kt /etc/security/keytabs/hbase.service.keytab hbase/$(hostname -f)@${kdc_realm}
+fi   
+
+
 echo "Creating Hbase tables..."
-kinit -kt /etc/security/keytabs/hbase.service.keytab hbase/$(hostname -f)@HWX.COM
 
 cat << EOF > /tmp/hbase.sh
 create 'T_PRIVATE','cf1','cf2'
@@ -13,8 +24,9 @@ hbase shell /tmp/hbase.sh
 
 echo "Creating Kafka topics..."
 
-kinit -kt /etc/security/keytabs/log_monitor.keytab log_monitor/$(hostname -f)@HWX.COM
-
+if [ "${enable_kerberos}" = true  ]; then   
+  kinit -kt /etc/security/keytabs/log_monitor.keytab log_monitor/$(hostname -f)@${kdc_realm}
+fi
 
 /usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --zookeeper $(hostname -f):2181 --replication-factor 1 --partition 1 --topic FOREX
 /usr/hdp/current/kafka-broker/bin/kafka-topics.sh --create --zookeeper $(hostname -f):2181 --replication-factor 1 --partition 1 --topic PRIVATE
@@ -50,14 +62,22 @@ EOF
 echo "Publishing test data to Kafka topics..."
 sleep 5
 
-/usr/hdp/current/kafka-broker/bin/kafka-console-producer.sh --security-protocol SASL_PLAINTEXT --broker-list $(hostname -f):6667 --topic PRIVATE < /tmp/private.csv
-/usr/hdp/current/kafka-broker/bin/kafka-console-producer.sh --security-protocol SASL_PLAINTEXT --broker-list $(hostname -f):6667 --topic FOREX <  /tmp/forex.csv
+if [ "${enable_kerberos}" = true  ]; then  
+   security_protocol=SASL_PLAINTEXT
+else
+   security_protocol=PLAINTEXT
+fi
+
+/usr/hdp/current/kafka-broker/bin/kafka-console-producer.sh --security-protocol ${security_protocol} --broker-list ${kafka_broker}:6667 --topic PRIVATE < /tmp/private.csv
+/usr/hdp/current/kafka-broker/bin/kafka-console-producer.sh --security-protocol ${security_protocol} --broker-list ${kafka_broker}:6667 --topic FOREX <  /tmp/forex.csv
 
 
-echo "Allowing Zeppelin to kinit as joe/ivanna.."
-chown joe_analyst:hadoop /etc/security/keytabs/joe_analyst.keytab
-chmod g+r /etc/security/keytabs/joe_analyst.keytab
+if [ "${enable_kerberos}" = true  ]; then 
+   echo "Allowing Zeppelin to kinit as joe/ivanna.."
+   chown joe_analyst:hadoop /etc/security/keytabs/joe_analyst.keytab
+   chmod g+r /etc/security/keytabs/joe_analyst.keytab
 
-chown ivanna_eu_hr:hadoop /etc/security/keytabs/ivanna_eu_hr.keytab
-chmod g+r /etc/security/keytabs/ivanna_eu_hr.keytab
+   chown ivanna_eu_hr:hadoop /etc/security/keytabs/ivanna_eu_hr.keytab
+   chmod g+r /etc/security/keytabs/ivanna_eu_hr.keytab
+fi
 
