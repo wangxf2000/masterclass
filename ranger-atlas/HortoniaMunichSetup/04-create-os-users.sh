daed58084d54462ea4588c7691a88683d2f94299
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2018-04-13 12:03:29 -0700

    added michelle_dpo

--- ranger-atlas/HortoniaMunichSetup/04-create-os-users.sh
+++ ranger-atlas/HortoniaMunichSetup/04-create-os-users.sh
@@ -1,26 +1,34 @@
-groupadd us_employee
-groupadd eu_employee
-groupadd analyst
-groupadd hr
-useradd -g analyst joe_analyst
-useradd -g hr kate_hr
-useradd -g hr sasha_eu_hr
-useradd -g hr ivanna_eu_hr
-usermod -a -G us_employee joe_analyst
-usermod -a -G us_employee kate_hr
-usermod -a -G eu_employee sasha_eu_hr
-usermod -a -G eu_employee ivanna_eu_hr
-
 groupadd finance
 groupadd business_dev
 groupadd contractor
 groupadd csr
 groupadd etl
-groupadd dpo
+
 useradd -g finance john_finance
 useradd -g business_dev mark_bizdev
-useradd -g contractor jermy_contractor
+useradd -g contractor jeremy_contractor
 useradd -g csr diane_csr
 useradd -g etl log_monitor
 useradd -g etl etl_user
+
+
+groupadd us_employee
+groupadd eu_employee
+groupadd analyst
+groupadd hr
+groupadd dpo
+
+useradd -g analyst joe_analyst
+useradd -g hr kate_hr
+useradd -g hr sasha_eu_hr
+useradd -g hr ivanna_eu_hr
 useradd -g dpo michelle_dpo
+
+#below users should also be added to us_employee or eu_employee
+usermod -a -G us_employee joe_analyst
+usermod -a -G us_employee kate_hr
+usermod -a -G eu_employee sasha_eu_hr
+usermod -a -G eu_employee ivanna_eu_hr
+usermod -a -G eu_employee michelle_dpo
+
+
