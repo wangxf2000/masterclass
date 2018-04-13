deed37a24684930b7098b84c434b94be30218d39
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2018-04-12 23:15:16 -0700

    Update 07-create-hive-schema-kerberos.sh

--- ranger-atlas/HortoniaMunichSetup/07-create-hive-schema-kerberos.sh
+++ ranger-atlas/HortoniaMunichSetup/07-create-hive-schema-kerberos.sh
@@ -1,5 +1,11 @@
 export kdc_realm=${kdc_realm:-HWX.COM}
+export beeline_url="jdbc:hive2://localhost:10000/default;principal=hive/$(hostname -f)@${kdc_realm}"
 
 kinit -kVt /etc/security/keytabs/hive.service.keytab hive/$(hostname -f)@${kdc_realm}
-beeline -u "jdbc:hive2://localhost:10000/default;principal=hive/$(hostname -f)@${kdc_realm}" -f ./data/HiveSchema.hsql
+beeline -u ${beeline_url} -f ./data/HiveSchema.hsql
+
+if [ "${enable_hive_acid}" = true  ]; then
+  beeline -u ${beeline_url -n hive -f data/TransSchema.hsql
+fi
+
 kdestroy
