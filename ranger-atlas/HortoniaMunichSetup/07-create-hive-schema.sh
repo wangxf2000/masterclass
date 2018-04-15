a0f3868fed48ae31634b2db7f7e51d4686ad7af4
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2018-04-16 01:18:05 +0200

    Update 07-create-hive-schema.sh

--- ranger-atlas/HortoniaMunichSetup/07-create-hive-schema.sh
+++ ranger-atlas/HortoniaMunichSetup/07-create-hive-schema.sh
@@ -1,6 +1,6 @@
 export beeline_url="jdbc:hive2://localhost:10000"
 
-beeline -u ${beeline_url} -f ./data/HiveSchema.hsql
+beeline -u ${beeline_url} -n hive -f ./data/HiveSchema.hsql
 
 if [ "${enable_hive_acid}" = true  ]; then
   beeline -u ${beeline_url} -n hive -f data/TransSchema.hsql
