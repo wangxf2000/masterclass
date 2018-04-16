37115c14747da48d8afa8520c0c73ad029f68ccd
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2018-04-16 12:13:00 +0200

    Replace log_monitor with michelle_dpo for Zeppelin login

--- ranger-atlas/setup.sh
+++ ranger-atlas/setup.sh
@@ -274,7 +274,7 @@ EOF
         | sed -e '1,2d' \
         -e "s/admin = admin, admin/etl_user = ${ambari_pass},admin/"  \
         -e "s/user1 = user1, role1, role2/ivanna_eu_hr = ${ambari_pass}, admin/" \
-        -e "s/user2 = user2, role3/log_monitor = ${ambari_pass}, admin/" \
+        -e "s/user2 = user2, role3/michelle_dpo = ${ambari_pass}, admin/" \
         -e "s/user3 = user3, role2/joe_analyst = ${ambari_pass}, admin/" \
         > /tmp/zeppelin-env.json
 
