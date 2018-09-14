3d0b02564bf6c04706449c40266b1d88ee76a4d6
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2018-09-14 16:17:45 -0700

    added HR data from employees.csv

--- ranger-atlas/HortoniaMunichSetup/06-copy-data-to-hdfs.sh
+++ ranger-atlas/HortoniaMunichSetup/06-copy-data-to-hdfs.sh
@@ -9,7 +9,8 @@ hdfs dfs -mkdir -p /hive_data/hortoniabank/us_customers/
 hdfs dfs -mkdir -p /hive_data/hortoniabank/ww_customers/
 hdfs dfs -mkdir -p /hive_data/consent/
 hdfs dfs -mkdir -p /hive_data/eu_countries/
-
+hdfs dfs -mkdir -p /hive_data/hr/employees_raw/
+hdfs dfs -mkdir -p /hive_data/hr/employees/
 
 
 hdfs dfs -put data/claims_provider_summary_data.csv /hive_data/claim/
@@ -22,7 +23,11 @@ hdfs dfs -put data/us_customers_data.csv            /hive_data/hortoniabank/us_c
 hdfs dfs -put data/ww_customers_data.csv            /hive_data/hortoniabank/ww_customers/
 hdfs dfs -put data/consent_master_data_cleaned.csv	/hive_data/consent/
 hdfs dfs -put data/eu_countries.csv	                /hive_data/eu_countries/
+hdfs dfs -put data/employees_raw.csv                /hive_data/hr/employees_raw/
+hdfs dfs -put data/employees.csv                    /hive_data/hr/employees/
 
 
 hdfs dfs -chown -R hive:hive /hive_data/
+
+
 
