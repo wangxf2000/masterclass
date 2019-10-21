0b8ea1a015b0690a7e6fc1c1a6fa1c6f0828ea47
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2019-10-21 10:13:36 -0700

    Update setup_cdp.sh

--- ranger-atlas/setup_cdp.sh
+++ ranger-atlas/setup_cdp.sh
@@ -4,10 +4,10 @@
 
 export env_name='testenv-01'       ## needed for Ranger/atlas API
 export datalake_name='testenv-01'  ## needed to create Ranger Admins role for group: cdp_<env_name>. This might be same as datalake_name
-export user='myuserid'              ## Admin user. make sure IPA password is set for this user first
+export user='etl_user'              ## Admin user (e.g. etl_user) make sure IPA password is set for this user first
 export password='BadPass#1'         ## replace with pasword
 export lake_knox='10.10.0.5'       ## private IP address of DataLake master node
-export s3bucket="s3a://mybucket/data"   ##replace with your data S3 bucket
+export s3bucket="s3a://mybucket/data"   ##replace with your data S3 bucket for dataeng
 
 #1. Confirm demo users/groups are in Ranger (e.g. joe_analyst, michelle_dpo, jeremy_contractor, diane_csr) else Ranger policy import will fail
 #2. Confirm that group cdp_env_admin_<env name> is created and sync'd to Ranger. Confirm above user is part of this group or he won't have admin rights
