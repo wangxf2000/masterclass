67b571cacd48a068590f11f209f416ab11fda467
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2019-10-23 19:40:32 -0700

    Update setup_cdp.sh

--- ranger-atlas/setup_cdp.sh
+++ ranger-atlas/setup_cdp.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 #set -o xtrace
 #Run from *DataEng* cluster with at least 2 worker nodes
-#Make sure to change below variables according to your environment
+#Make sure to change below variables according to your own environment
 
 export env_name='testenv-01'       ## needed for Ranger/atlas API
 export datalake_name='testenv-01'  ## needed to create Ranger Admins role for group: cdp_<env_name>. This might be same as datalake_name
