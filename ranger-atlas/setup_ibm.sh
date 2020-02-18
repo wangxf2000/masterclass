9a1fcabb1686ff3e3f51ae416bd0b9e70c2b9d32
Ali Bajwa <abajwa-hw@users.noreply.github.com>
2020-02-18 13:14:40 -0800

    Update setup_ibm.sh

--- ranger-atlas/setup_ibm.sh
+++ ranger-atlas/setup_ibm.sh
@@ -23,7 +23,10 @@ if [ $(rpm --query centos-release | grep "not installed" | wc -l) == 1 ]; then
   #rest of the workshop vm install commands RH or centos:
   #install tools
   yum install -y java-1.8.0-openjdk-devel vim wget curl git bind-utils chrony
+else
+  yum install -y git
 fi
+
 
 echo "Setting up KDC..."
 curl -sSL https://gist.github.com/abajwa-hw/bca3d23fe146c3ebd59a9b5fd19480a3/raw | sudo -E sh
@@ -34,6 +37,7 @@ git clone https://github.com/fabiog1901/SingleNodeCDPCluster.git
 
 cd SingleNodeCDPCluster
 ./setup_krb.sh gcp templates/wwbank_krb.json
+
 
 echo "Setting up Worldwide bank demo..."
 curl -sSL https://raw.githubusercontent.com/abajwa-hw/masterclass/master/ranger-atlas/setup-dc-703.sh | sudo -E bash
